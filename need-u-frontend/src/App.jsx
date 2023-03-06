6a73b7bfd47609d0d5a2ea6cfe827d30a273158f
CHO <whdbscks77@gmail.com>
2023-03-06 15:10:52 +0900

    board(게시판 레이아웃 작업 완료)

---
+++ need-u-frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import Home from './component/home_structure/home.js'
+import Board from './component/board_structure/board.js'
+import store from './home_store.js'
+import './App.css';
+import React, { Component, useState } from "react";
+
+
+export default class App extends Component{
+
+    state = {number:store.getState().number} 
+
+    constructor(props){ 
+      super(props);
+      store.subscribe(function(){ // subscribe는 store가 변할때 마다 호출이 된다.
+          this.setState({number:store.getState().number});
+      }.bind(this));
+    }
+
+    render() { 
+    return (
+      <div className="App">
+
+        {this.state.number !== 5 && <Home></Home>}
+        {this.state.number === 5 && <Board></Board>}             
+      </div>
+    );
+  }
+}
+
